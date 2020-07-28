b749230221e7074a65c83cb49db9f09d8d80eda0
Keyur Vadodariya <vadodariyakeyur321@gmail.com>
2020-07-28 22:08:00 +0530

    pROJECT sETUP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>COVID-19 TRACKER</h1>
+
+      {/*Header */}
+      {/*Title + Select Dropdown field */}
+
+      {/*Info Boxes */}
+      {/*Info Boxes */}
+      {/*Info Boxes */}
+
+      {/*Table */}
+      {/*Graph */}
+
+      {/*Map */}
     </div>
   );
 }
