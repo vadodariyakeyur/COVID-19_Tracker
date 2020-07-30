1f50bf443592a2e133cb0af0fea1485900447cde
Keyur Vadodariya <vadodariyakeyur321@gmail.com>
2020-07-30 12:56:55 +0530

    Map Working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,108 @@
-import React from 'react';
+import React , {useState, useEffect} from 'react';
+import {MenuItem, FormControl, Select, Card, CardContent} from '@material-ui/core';
+import InfoBox from "./InfoBox";
+import Table from "./Table";
+import Map from "./Map";
+import LineGraph from "./LineGraph"
+import {sortData} from "./util";
 import './App.css';
+import 'leaflet/dist/leaflet.css';
 
 function App() {
+
+  const [countries,setCountries] = useState([]);
+  const [country,setCountry] = useState('worldwide');
+  const [countryInfo,setCountryInfo] = useState({});
+  const [tableData,setTableData] = useState([]);
+  const [mapCenter,setMapCenter] = useState({lat: 34.80746,lng: -40.4796});
+  const [mapZoom,setMapZoom] = useState(3);
+  const [mapCountries,setMapCountries] = useState([]);
+
+  useEffect( () => {
+
+    fetch(`https://disease.sh/v3/covid-19/all`)
+    .then(response => response.json())
+    .then(data => {
+      setCountryInfo(data);
+    });
+
+  }, [])
+
+  useEffect( () => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => ({
+          name: country.country,
+          value: country.countryInfo.iso2
+        }));
+
+        const sortedData = sortData(data);
+        setTableData(sortedData);
+        setMapCountries(data);
+        setCountries(countries);
+      });
+    };
+
+    getCountriesData();
+  },[]);
+
+  const onCountryChange = async(event) => {
+    const countryCode = event.target.value;
+
+    const url = countryCode === "worldwide" ? `https://disease.sh/v3/covid-19/all` : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      setCountry(countryCode);
+      setCountryInfo(data);
+
+      setMapCenter([data.countryInfo.lat,data.countryInfo.long])
+      setMapZoom(4)
+    });
+  }
+
   return (
     <div className="app">
-      <h1>COVID-19 TRACKER</h1>
+      
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app_dropdown">
+            <Select varient="outlined" value={country} onChange={onCountryChange}>
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              { countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
 
-      {/*Header */}
-      {/*Title + Select Dropdown field */}
+        <div className="app_stats">
+          <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases}/>
+          <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered}/>
+          <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths}/>
+        </div>
+        
+        <Map 
+          countries={mapCountries}
+          center={mapCenter}
+          zoom={mapZoom}
+        />
 
-      {/*Info Boxes */}
-      {/*Info Boxes */}
-      {/*Info Boxes */}
+      </div>
 
-      {/*Table */}
-      {/*Graph */}
-
-      {/*Map */}
+      <Card className="app_right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <Table countries={tableData}/>
+          <h3>Worldwide new cases</h3>
+          <LineGraph />
+        </CardContent>
+      </Card>
+      
     </div>
   );
 }
---
+++ src/LineGraph.js
@@ -0,0 +1,100 @@
+import React, {useState, useEffect} from 'react';
+import {Line} from 'react-chartjs-2';
+import numeral from 'numeral';
+
+const options = {
+    legend: {
+        display: false
+    },
+    elements: {
+        point: {
+            radius: 0
+        },
+    },
+    maintainAspectRatio: false,
+    tooltips: {
+        mode: "index",
+        intersect: false,
+        callbacks: {
+            label: function (tooltipItem,data){
+                return numeral(tooltipItem.value).format("+0,0")
+            }
+        }
+    },
+    scales: {
+        xAxes: [
+            {
+                type: "time",
+                time: {
+                    format: "MM/DD/YY",
+                    tooltipFormat: "ll",
+                }   
+            }
+        ],
+        yAxes: [
+            {
+                gridLines: {
+                    display: false,
+                },
+                ticks: {
+                    callback: function(value,index,values){
+                        return numeral(value).format("0a")
+                    }
+                }
+            }
+        ]
+    }
+}
+
+function LineGraph({ casesType = 'cases'}) {
+
+    const [data,setData] = useState({})
+
+    const buildChartData = (data) => {
+        const chartData = [];
+        let lastDataPoint;
+        
+        for(let date in data[casesType]){
+            if (lastDataPoint)
+            {
+                const newDataPoint = {
+                    x: date,
+                    y: data[casesType][date] - lastDataPoint
+                }
+                chartData.push(newDataPoint);
+            }
+            lastDataPoint = data[casesType][date];
+        }
+        return chartData
+    }
+
+    useEffect(() => {
+        const fetchData = async() => {
+            await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120').then(response => response.json())
+            .then(data => {
+                const chartData = buildChartData(data);
+                setData(chartData);
+            })
+        };
+        fetchData();
+    },[]);
+
+    return (
+        <div>
+            {data?.length > 0 && (
+                <Line 
+                    options= {options}
+                    data = {{
+                        datasets: [{
+                            backgroundColor: "rgba(240,16,52,0.7)",
+                            borderColor: "#CC1034",
+                            data: data
+                        }]
+                    }}
+                />
+            )}
+        </div>
+    )
+}
+
+export default LineGraph
